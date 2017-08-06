0b166a96a674fc48cba1dc513d54311f31c96d8a
smuliava <s.muliava@gmail.com>
2017-08-06 11:59:15 +0300

    Start of implementation of system menu (#2)
    Start of System menu implementation

---
+++ client/src/app/components/system-menu/system-menu.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'sid-system-menu',
+    templateUrl: './system-menu.component.html',
+    styleUrls: ['./system-menu.component.scss']
+})
+export class SystemMenuComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
