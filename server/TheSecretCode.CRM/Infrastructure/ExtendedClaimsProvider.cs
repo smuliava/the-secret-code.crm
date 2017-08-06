ee71521d18721fb5ac241e13ec194e81763944e2
smuliava <s.muliava@gmail.com>
2017-08-06 10:52:51 +0300

    Authentification (#1)
    * Renamed solution
    * Added Jwt token support
    * Authentification configuration
    * Authentication configuration

---
+++ server/TheSecretCode.CRM/Infrastructure/ExtendedClaimsProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using TheSecretCode.CRM.Infrastructure;
+
+namespace TheSecretCode.CRM.Providers
+{
+    public class ExtendedClaimsProvider
+    {
+        public static IEnumerable<Claim> GetClaims(SystemUser user)
+        {
+
+            List<Claim> claims = new List<Claim>();
+
+            var daysInWork = (DateTime.Now.Date - user.RegistrationDate).TotalDays;
+
+            if (daysInWork > 90)
+            {
+                claims.Add(CreateClaim("FTE", "1"));
+
+            }
+            else
+            {
+                claims.Add(CreateClaim("FTE", "0"));
+            }
+
+            return claims;
+        }
+
+        public static Claim CreateClaim(string type, string value)
+        {
+            return new Claim(type, value, ClaimValueTypes.String);
+        }
+    }
+}
---
+++ server/TheSecretCode.CRM/Infrastructure/SystemUser.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TheSecretCode.CRM.Infrastructure
+{
+    public class SystemUser : IdentityUser
+    {
+        //[MaxLength(100)]
+        //public string FirstName { get; set; }
+
+        //[MaxLength(100)]
+        //public string LastName { get; set; }
+
+        //[MaxLength(100)]
+        //public string MiddleName { get; set; }
+
+        //public byte Level { get; set; }
+
+        [Required]
+        public DateTime RegistrationDate { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<SystemUser> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+
+            return userIdentity;
+        }
+    }
+}
