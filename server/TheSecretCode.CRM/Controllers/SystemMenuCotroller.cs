41253ac5220cdd57ecfdd3d020f03cc5b2b73a4f
Ivan Zagoruiko <Iv.Zagoruiko@gmail.com>
2017-08-09 23:43:10 +0300

    Start of implementation of system menu

---
+++ server/TheSecretCode.CRM/Controllers/SystemMenuCotroller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace TheSecretCode.CRM.Controllers
+{
+    [RoutePrefix("api/SystemMenu")]
+    public class SystemMenuCotroller : ApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("")]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ server/TheSecretCode.CRM/Models/SystemMenuModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheSecretCode.CRM.Models
+{
+    public class SystemMenuModel
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Caption { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Order { get; set; }
+    }
+}
